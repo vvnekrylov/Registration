969b51c96ac38720b2a5a66f553c2c99b2b80bcb
vvnekrylov <72132940+vvnekrylov@users.noreply.github.com>
2020-11-12 18:30:54 +0300

    Add files via upload

--- form.php
+++ form.php
@@ -2,12 +2,15 @@
 $fd=fopen("accounts.txt",'a+') or die("Не удалось открыть файл");
 $login = 0;
 $password = 0;
+$tab="  ";
 if (isset($_POST['login']) && isset($_POST['password'])) 
     {
-        $login = "login";
-        $password = "password";
+        $login = $_POST['login'];
+        $password = $_POST['password'];
         fwrite($fd, $login);
+        fwrite($fd, $tab);
         fwrite($fd, $password);
+        header('Location:accept.html');
     }
 else 
     {
